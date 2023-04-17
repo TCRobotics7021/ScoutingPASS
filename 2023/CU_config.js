d5289a360de6ecdc8d6e931b959d535e1844c8db
MLakowske <44869298+MLakowske@users.noreply.github.com>
2023-04-17 07:07:59 -0500

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -1,7 +1,7 @@
 var config_data = `
 {
   "title": "Scouting PASS 2023",
-  "page_title": "Charged Upz",
+  "page_title": "Charged Up",
   "checkboxAs": "10",
   "prematch": [
     { "name": "Scouter Initials",
